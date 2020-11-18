a1a07f7dca86c8b9ffaa877c415bc764fb72715a
vkaszuba <codeword.vika@gmail.com>
2020-11-18 01:45:04 -0800

    Pokemon types added

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,20 @@
 const searchBtn = document.querySelector("#searchBtn");
 searchBtn.addEventListener("click", fetchPokemon);
 
+// Keeps page from refreshing after form submit
+let searchForm = document.getElementById("searchForm");
+searchForm.addEventListener('submit', handleSearchForm);
+function handleSearchForm(event) {
+  event.preventDefault();
+}
 
 // Pokemon API info fetch
 function fetchPokemon() {
 
-  let pokeName = "dratini";
+  // Grabs search bar info and inserts it into API
+  let searchName = document.getElementById("searchBar").value;
+  let pokeName = searchName.toLowerCase();
+
   let url = `https://pokeapi.co/api/v2/pokemon/${pokeName}/`;
 
   // Call the fetch function passing the url of the API as a parameter
@@ -18,14 +27,29 @@ function fetchPokemon() {
     })
     // Your code for handling the data you get from the API
     .then(function (results) {
-      console.log(results.name);
+      console.log(results);
 
       // Links up Pokemon name display
       let displayName = document.getElementById("displayName");
-      displayName.innerHTML = `${results.name.toUpperCase()}`;
+      displayName.innerHTML = `${results.name.charAt(0).toUpperCase() + results.name.slice(1)}`;
+
+
+      for (let i = 0; i < results.types.length; i++) {
+        // Links up Pokemon type for display
+        let displayType = document.getElementById("displayType");
+
+        // Need to loop through additional types, if they should exist
+        displayType.innerHTML += ` ${results.types[i].type.name.charAt(0).toUpperCase() + results.types[i].type.name.slice(1)} `;
+
+        console.log(results.types[i].type.name);
+      };
 
       // Links up Pokemon ID for sprite display
       document.getElementById("displaySprite").src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${results.id}.png`;
+    })
+    
+    .catch(function (error) {
+      console.log(error);
     });
 
 };
