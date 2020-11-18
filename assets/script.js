87dc8741f7ddaa8f080f539852dacc3cf7760701
vkaszuba <codeword.vika@gmail.com>
2020-11-17 23:15:54 -0800

    Set up correct sprite to appear with pokemon name

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
 
-// Button that creates a name for textbox
+// Button that searches for a pokemon
 const searchPokemon = document.querySelector("#searchPokemon");
 searchPokemon.addEventListener("click", fetchPokemon);
 
+
+// Pokemon API info fetch
 function fetchPokemon() {
 
-  let url = `https://pokeapi.co/api/v2/pokemon/butterfree/`
+  let pokeName = "dratini"
+  let url = `https://pokeapi.co/api/v2/pokemon/${pokeName}/`
 
   // Call the fetch function passing the url of the API as a parameter
   fetch(url)
@@ -16,27 +19,13 @@ function fetchPokemon() {
     // Your code for handling the data you get from the API
     .then(function (results) {
       console.log(results);
+      console.log(results.name);
+      console.log(results.id);
+
       let displayPokemon = document.querySelector("#displayPokemon");
       displayPokemon.value = results.name;
-    })
 
-};
-
-function fetchSprite() {
-
-let url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/700.png'
-
-  // Call the fetch function passing the url of the API as a parameter
-  fetch(url)
-    // Transform the data into json
-    .then(function (response) {
-      return response.json();
-    })
-    // Your code for handling the data you get from the API
-    .then(function (results) {
-      console.log(results);
-      let displayPokemon = document.querySelector("#displayPokemon");
-      displayPokemon.value = results.name;
-    })
+      document.getElementById("displaySprite").src=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${results.id}.png`;
+    });
 
 };
